3c06dc0866cea107aca805003c1041fb277e7b19
jonata <jonatanery2016@gmail.com>
2023-11-26 21:51:46 +0000

    script finalizado e com o crontab funcionando

--- nfs_server/script-atividade.sh
+++ nfs_server/script-atividade.sh
@@ -4,7 +4,7 @@
 NFS_DIRETORIO="/nfs_server/jonata_nery"
 
 #nome do serviço
-SERVICO="nfs-server"
+SERVICO="httpd"
 
 #verifica o status do serviço
 STATUS=$(systemctl is-active $SERVICO)
@@ -18,8 +18,13 @@ else
 	ARQUIVO_SAIDA="$NFS_DIRETORIO/${SERVICO}_offline.txt"
 fi
 
+#passa a data exata de quando o script foi executado pra uma constante
+DATA_HORA=$(date +"%Y-%m-%d %H:%M:%S")
+
 #cria o arquivo de saida com informaçoes
+echo "Data/Hora: $DATA_HORA" > "$ARQUIVO_SAIDA"
 echo "Serviço: $SERVICO" >> "$ARQUIVO_SAIDA"
 echo "Status: $STATUS" >> "$ARQUIVO_SAIDA"
 echo "Mensagem: $mensagem" >> "$ARQUIVO_SAIDA"
+echo "  "
 
